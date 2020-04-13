51f41124042e1ba187ba0e446355db402494d82f
zhaojinxin <474969612@qq.com>
2020-04-13 17:22:02 +0800

    修改配置

--- src/api/ContentController.js
+++ src/api/ContentController.js
@@ -3,6 +3,27 @@ import Post from '../modle/Post'
 class ContentController {
     async getPostList (ctx) {
         const body = ctx.query
+
+        const post = new Post({
+            title: 'test title',
+            content: 'test content',
+            catalog: 'ask',
+            fav: 20,
+            isEnd: '0',
+            reads: '0',
+            answer: '0',
+            status: '0',
+            isTop: '0',
+            sort: '0',
+            tags: [{
+                name: '精华',
+                class: ''
+            }]
+        })
+
+        const tmp = await post.save()
+        console.log(tmp)
+
         const sort = body.sort ? body.sort : 'created'
         const page = body.page ? parseInt(body.page) : 0
         const limit = body.limit ? parseInt(body.limit) : 20
@@ -19,7 +40,16 @@ class ContentController {
         if (typeof body.isEnd !== 'undefined') {
             options.isEnd = body.isEnd
         }
-        await Post.getList(options, sort, page, limit)
+        if (typeof body.tag !== 'undefined' && body.tag !== '') {
+            options.tags = { $elemMatch: {name: body.tag} }
+        }
+        const result = await Post.getList(options, sort, page, limit)
+
+        ctx.body = {
+            code: 200,
+            data: result,
+            msg: '获取文章列表成功'
+        }
     }
 }
 
--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
-const DB_URL = '192.168.1.114:27017/testdb'
+const DB_URL = '121.36.140.182:27017/testdb'
 const REDIS = {
-  host: '192.168.1.114',
-  port: 15001,
-  password: '123456'
+  host: '121.36.140.182',
+  port: 6379,
+  password: ''
 }
 const JWT_SECRET = 'a&*38QthAKuiRwISGLotgq^3%^$zvA3A6Hfr8MF$jM*HY4*dWcwAW&9NGp7*b53!'
 
